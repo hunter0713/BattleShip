960a1c21dc13fbdaca887643d05d9c548aafa6f0
Brandon Wheat <brandonwheat_email@ku.edu>
2019-10-16 14:08:30 -0500

    added comments

--- main.cpp
+++ main.cpp
@@ -9,12 +9,21 @@
 * \Brief:  File is main file
 * \copyright: Group "Big SegFault Energy" All rights reserved
 */
-
+/**
+* \Author: Hunter Cobb
+* \Author: Zackariah Khazraeinazmpour
+* \Author: Brandon Wheat
+* \Author: Justin Khounsombath
+* \Author: William Burdick
+* \File:	 Executive.cpp
+* \Date:   10/20/2019
+* \Brief:  main file that starts game.
+*/
 #include <iostream>
 #include "Executive.h"			//include executive for game functions
 
 int main()
-{	
+{
 	Executive exec;	//create executive object to utilize all game functions
 	return 0;				//exit program
 }
